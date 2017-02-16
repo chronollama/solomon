7cbe333a9b1efc88fef1c420898b0469c751fa5e
Jeffrey Wu <jwu.mathscience@gmail.com>
2017-02-16 16:23:47 -0500

    Add friendships controller and routes

---
+++ app/controllers/api/friendships_controller.rb
@@ -0,0 +1,34 @@
+class FriendshipsController < ApplicationController
+
+  # TODO: should this be params[:id] or nested params[:friend][:id]
+  def create
+    @user = User.find_by(email: friend_params)
+    if @user
+      @friendship = Friendship.new(user_id: current_user.id, friend_id: @user.id)
+      if @friendship.save
+        render 'api/users/show'
+      else
+        render json: @friendship.errors.full_messages, status: 422
+      end
+
+    else
+      render json: @user.errors.full_messages, status: 404
+    end
+  end
+
+  def destroy
+    @friendship = Friendship.find_by(user_id: user_id, friend_id: params[:id])
+    @friendship ||= Friendship.find_by(user_id: params[:id], friend_id: current_user.id)
+    if @friendship
+      @friendship.destroy
+      redirect_to root_url
+    else
+      render json: @friendship.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def friend_params
+    params.require(:friend).permit(:email)
+  end
+end
