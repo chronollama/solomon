edf1c1198b49bc99fe1942250b0cc62fb9d1dcce
Jeffrey Wu <jwu.mathscience@gmail.com>
2017-02-22 22:26:58 -0500

    Add bill seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,12 @@ Friendship.create(user_id: user4.id, friend_id: user6.id)
 Friendship.create(user_id: user5.id, friend_id: user6.id)
 Friendship.create(user_id: user3.id, friend_id: user4.id)
 
-Bill.create(category: 'entertainment', description: 'Star Wars XIV', total: 13, date: Date.parse('28-12-2020'), notes: 'Luke is his own father!')
-Bill.create(description: 'Chipotle', total: 8.50, date: Date.parse('12-2-2017'))
-# Bill.new(category: 'food', description: 'donuts', total: 8, date: Date.parse('16-2-2017'), notes: 'sprinkles!')
+bill1 = Bill.new(category: 'entertainment', description: 'Star Wars XIV', total: 50, date: Date.parse('28-12-2020'), notes: 'Luke is his own father!')
+bill2 = Bill.new(description: 'Chipotle', total: 26, date: Date.parse('12-2-2017'))
+bill3 = Bill.new(category: 'food', description: 'donuts', total: 8, date: Date.parse('16-2-2017'), notes: 'sprinkles!')
+shares1 = {user1.id => 0, user2.id => 50,user3.id => 0,user4.id => 0}
+shares2 = {user1.id => 2, user2.id => 10,user3.id => 5,user4.id => 9}
+shares3 = {user1.id => 5, user2.id => 3, user3.id => 0, user4.id => 0}
+bill1.make_records(shares1)
+bill2.make_records(shares2)
+bill3.make_records(shares3)
